089a5b63c7d6d4bcbc4763a219b70d26085be88c
Aaronsms78 <aaronsms@u.nus.edu>
2020-06-02 15:00:33 +0800

    Formatting changes

--- index.js
+++ index.js
@@ -1,97 +1,93 @@
-let express = require('express')
-let jwt = require('jsonwebtoken')
-let spreadsheet = require('./spreadsheet')
-let app = express()
-
+let express = require("express");
+let jwt = require("jsonwebtoken");
+let spreadsheet = require("./spreadsheet");
+let app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
+app.get("/api", (req, res) => res.send("Hello World"));
 
-app.get('/api', (req, res) => res.send('Hello World'))
-
-app.get('/api/leaderboard',
-    (req, res) => spreadsheet.leaderboard()
-        .then(obj => res.send(JSON.stringify(obj))));
+app.get("/api/leaderboard", (req, res) =>
+  spreadsheet.leaderboard().then((obj) => res.send(JSON.stringify(obj)))
+);
 
 // Process puzzle answer and update scores
 // TODO: update redirect links and error handling, include answer checking
-app.post('/api/solve', verifyGroup, 
-	 (req, res) => {
-             jwt.verify(req.token, 'secretkey', 
-		        (err, groupData) => {
-			    if (err) {
-				res.status(403).json({ msg: 'error' });
-			    } else {
-				spreadsheet.solved(req.puzzleName, groupData.group)
-					   .then(() => { res.send(groupData); });
-			    }
-			});
-		 
+app.post("/api/solve", verifyGroup, (req, res) => {
+  jwt.verify(req.token, "secretkey", (err, groupData) => {
+    if (err) {
+      res.status(403).json({ msg: "error" });
+    } else {
+      spreadsheet.solved(req.puzzleName, groupData.group).then(() => {
+        res.send(groupData);
+      });
+    }
+  });
 });
 
 // Group registration
 // TODO: update redirect link
-app.post('/api/signup', (req, res) => {
-    const newGroup = {
-        Name: req.body.name,
-        Password: req.body.password,
-    };
+app.post("/api/signup", (req, res) => {
+  const newGroup = {
+    Name: req.body.name,
+    Password: req.body.password,
+  };
 
-    if (!newGroup.Name || !newGroup.Password) {
-        res.status(400).json({ msg: 'Please include a name or a password' }); 
-    } else {
-	spreadsheet.authenticate(newGroup)
-	           .then(x => {
-		       if (x) {
-		            res.status(400).json({ msg: 
-			        'This team name is already registered. Please login or get a new team name' 
-			    });
-		       } else {
-                           spreadsheet.save(newGroup).then(x => res.send(JSON.stringify(x)));
-	                   res.redirect("http://nusmsl.com/hunt/map.html");
-		       }
-		   });
-    }
+  if (!newGroup.Name || !newGroup.Password) {
+    res.status(400).json({ msg: "Please include a name or a password" });
+  } else {
+    spreadsheet.authenticate(newGroup).then((x) => {
+      if (x) {
+        res.status(400).json({
+          msg:
+            "This team name is already registered. Please login or get a new team name",
+        });
+      } else {
+        spreadsheet.save(newGroup).then((x) => res.send(JSON.stringify(x)));
+        res.redirect("http://nusmsl.com/hunt/map.html");
+      }
+    });
+  }
 });
 
 // Group login
-app.post('/api/login', (req, res) => {
-    const group = {
-        Name: req.body.name,
-        Password: req.body.password,
-    };
+app.post("/api/login", (req, res) => {
+  const group = {
+    Name: req.body.name,
+    Password: req.body.password,
+  };
 
-    if (!group.Name || !group.Password) {
-        res.status(400).json({ msg: 'Please enter your name and password' });
-    } else {
-        spreadsheet.verify(group)
-		   .then(x => {
-			     if (x) {
-			         jwt.sign({ group }, 'secretkey', 
-				          (err, token) => { 
-				              res.json({ token });
-				          });
-			     } else {
-			         res.status(400).json({ msg: 'Invalid name/password. Please try again or register' });
-			     }
-	                 });
-    }
+  if (!group.Name || !group.Password) {
+    res.status(400).json({ msg: "Please enter your name and password" });
+  } else {
+    spreadsheet.verify(group).then((x) => {
+      if (x) {
+        jwt.sign({ group }, "secretkey", (err, token) => {
+          res.json({ token });
+        });
+      } else {
+        res
+          .status(400)
+          .json({ msg: "Invalid name/password. Please try again or register" });
+      }
+    });
+  }
 });
 
-// Verify group token 
+// Verify group token
 function verifyGroup(req, res, next) {
-    const header = req.headers['authorization'];
-    // If header is undefined send error, 
-    // else get token and call the next middleware
-    if (typeof header !== undefined) {
-	const bearer = header.split(' ');
-	const token = bearer[1];
-	req.token = token;
-	next();
-    } else {
-	res.status(403).json({ msg: 'Please login to submit your answer' });
-    }
+  const header = req.headers["authorization"];
+  // If header is undefined send error,
+  // else get token and call the next middleware
+  if (typeof header !== undefined) {
+    const bearer = header.split(" ");
+    const token = bearer[1];
+    req.token = token;
+    next();
+  } else {
+    res.status(403).json({ msg: "Please login to submit your answer" });
+  }
 }
 
-app.listen(3000, () => console.log('Server running at Port 3000'))
+app.listen(3000, () => console.log("Server running at Port 3000"));
--- spreadsheet.js
+++ spreadsheet.js
@@ -1,5 +1,5 @@
-const { GoogleSpreadsheet } = require('google-spreadsheet');
-const key = require('./server-key.json');
+const { GoogleSpreadsheet } = require("google-spreadsheet");
+const key = require("./server-key.json");
 
 const CREDENTIALS_ID = 0;
 const LEADERBOARD_ID = 1618630879;
@@ -7,130 +7,136 @@ const STATISTICS_ID = 1981752792;
 const GROUPS_ID = [];
 
 async function init() {
-    const doc = new GoogleSpreadsheet('1vYE0U_GbDKHW5ZZsUqt4d6-3UOYeehwM8tWS5VQxTbE');
-    await doc.useServiceAccountAuth(key);
-    await doc.loadInfo();
-    return doc;
+  const doc = new GoogleSpreadsheet(
+    "1vYE0U_GbDKHW5ZZsUqt4d6-3UOYeehwM8tWS5VQxTbE"
+  );
+  await doc.useServiceAccountAuth(key);
+  await doc.loadInfo();
+  return doc;
 }
 
 async function total(doc) {
-    const sheet = doc.sheetsById[STATISTICS_ID];
-    await sheet.loadCells();
-    return sheet.getCellByA1('B1').value;
+  const sheet = doc.sheetsById[STATISTICS_ID];
+  await sheet.loadCells();
+  return sheet.getCellByA1("B1").value;
 }
 
 async function accessSheet(sheet, total) {
-    await sheet.loadHeaderRow();
-    await sheet.loadCells();
-    const rows = await sheet.getRows({offset: 0, limit: total});
-    const headers = sheet.headerValues;
+  await sheet.loadHeaderRow();
+  await sheet.loadCells();
+  const rows = await sheet.getRows({ offset: 0, limit: total });
+  const headers = sheet.headerValues;
 
-    let JSON = [];
-    for (const row of rows) {
-        let obj = {};
-        for (let i = 0; i < headers.length; i++) {
-            obj[headers[i]] = row[headers[i]];
-        }
-        JSON.push(obj);
+  let JSON = [];
+  for (const row of rows) {
+    let obj = {};
+    for (let i = 0; i < headers.length; i++) {
+      obj[headers[i]] = row[headers[i]];
     }
-    return JSON;
+    JSON.push(obj);
+  }
+  return JSON;
 }
 
 async function accessCredentials() {
-    const doc = await init();
-    const totalGroups = await total(doc);
-    const sheet = doc.sheetsById[CREDENTIALS_ID];
-    return accessSheet(sheet, totalGroups);
+  const doc = await init();
+  const totalGroups = await total(doc);
+  const sheet = doc.sheetsById[CREDENTIALS_ID];
+  return accessSheet(sheet, totalGroups);
 }
 
 async function accessLeaderboard() {
-    const doc = await init();
-    const totalGroups = await total(doc);
-    const sheet = doc.sheetsById[LEADERBOARD_ID];
-    return accessSheet(sheet, totalGroups);
+  const doc = await init();
+  const totalGroups = await total(doc);
+  const sheet = doc.sheetsById[LEADERBOARD_ID];
+  return accessSheet(sheet, totalGroups);
 }
 
 async function save(newGroup) {
-    const doc = await init();
-    const totalGroups = await total(doc);
-    newGroup.ID = totalGroups + 1;
-    await doc.sheetsById[CREDENTIALS_ID].addRow(newGroup);
+  const doc = await init();
+  const totalGroups = await total(doc);
+  newGroup.ID = totalGroups + 1;
+  await doc.sheetsById[CREDENTIALS_ID].addRow(newGroup);
 
-    const newSheet = await doc.addSheet({
-        title: `Group ${newGroup.ID}`, index: 2 + newGroup.ID });
-    await newSheet.setHeaderRow(["Puzzles", "Solved", "Score"]);
-    GROUPS_ID[newGroup.ID - 1] = newSheet.sheetId;
+  const newSheet = await doc.addSheet({
+    title: `Group ${newGroup.ID}`,
+    index: 2 + newGroup.ID,
+  });
+  await newSheet.setHeaderRow(["Puzzles", "Solved", "Score"]);
+  GROUPS_ID[newGroup.ID - 1] = newSheet.sheetId;
 
-    const sheet = await doc.sheetsById[LEADERBOARD_ID];
-    await sheet.loadCells('C1:C30');
-    sheet.getCell(newGroup.ID, 2).formula = `=IF(ISBLANK(A:A),"",0 + SUM(\'Group ${newGroup.ID}\'!C:C))`;
-    await sheet.saveUpdatedCells();
-    return newGroup;
+  const sheet = await doc.sheetsById[LEADERBOARD_ID];
+  await sheet.loadCells("C1:C30");
+  sheet.getCell(
+    newGroup.ID,
+    2
+  ).formula = `=IF(ISBLANK(A:A),"",0 + SUM(\'Group ${newGroup.ID}\'!C:C))`;
+  await sheet.saveUpdatedCells();
+  return newGroup;
 }
 
 // Checks the group login details
 async function verify(group) {
-    const doc = await init();
-    const totalGroups = await total(doc);
-    const JSON = await accessCredentials();
-    let pass = false;
+  const JSON = await accessCredentials();
+  let pass = false;
 
-    for (let i =0; i < JSON.length; i++) {
-        if (group.Name === JSON[i].Name && group.Password === JSON[i].Password) {
-	    pass = true;
-	}
+  for (let i = 0; i < JSON.length; i++) {
+    if (group.Name === JSON[i].Name && group.Password === JSON[i].Password) {
+      pass = true;
     }
+  }
 
-    return pass;
+  return pass;
 }
 
 // Checks if the group is already registered
 async function isRegistered(group) {
-    const doc = await init();
-    const totalGroups = await total(doc);
-    const JSON = await accessCredentials();
-    var registered = false;
- 
-    for (let i = 0; i < JSON.length; i++) {
-        if (group.Name === JSON[i].Name) {
-	    registered = true;
-	} else {}
-    }
+  const JSON = await accessCredentials();
+  var registered = false;
 
-    return registered;
+  for (let i = 0; i < JSON.length; i++) {
+    if (group.Name === JSON[i].Name) {
+      registered = true;
+    } else {
+    }
+  }
+
+  return registered;
 }
 
 // Checks the answer submitted by the group to the puzzle with name puzzleName and
-// updates the group sheet 
+// updates the group sheet
 async function solved(puzzleName, group) {
-    const doc = await init();
-    const totalGroups = await total(doc);
-    const JSON = await accessCredentials();
+  const JSON = await accessCredentials();
 
-    let i = 0;
-    let groupFound = false;
-    while (i < JSON.length && !groupFound) {
-	groupFound = JSON[i].Name === group.Name;
-	if (!groupFound) {
-            i++;
-	} else {}
+  let i = 0;
+  let groupFound = false;
+  while (i < JSON.length && !groupFound) {
+    groupFound = JSON[i].Name === group.Name;
+    if (!groupFound) {
+      i++;
     }
+  }
 
-    console.log(groupFound);
-    if (groupFound) {
-	const groupSheet = await accessGroup(i + 1);
-	await groupSheet.addRow({ "Puzzles": puzzleName, "Solved": 'TRUE', "Score": 1 });
-    } else {}
+  console.log(groupFound);
+  if (groupFound) {
+    const groupSheet = await accessGroup(i + 1);
+    await groupSheet.addRow({ Puzzles: puzzleName, Solved: "TRUE", Score: 1 });
+  }
 }
 
 // Gets the group sheet by id
 async function accessGroup(groupId) {
-    const doc = await init();
-    const totalGroups = await total(doc);
-    const sheet = await doc.sheetsById[GROUPS_ID[groupId - 1]];
-    return sheet;
+  const doc = await init();
+  const sheet = await doc.sheetsById[GROUPS_ID[groupId - 1]];
+  return sheet;
 }
 
-
-module.exports = {credentials: accessCredentials, leaderboard: accessLeaderboard, save: save, verify: verify,
-		  authenticate: isRegistered, solved: solved};
+module.exports = {
+  credentials: accessCredentials,
+  leaderboard: accessLeaderboard,
+  save: save,
+  verify: verify,
+  authenticate: isRegistered,
+  solved: solved,
+};
